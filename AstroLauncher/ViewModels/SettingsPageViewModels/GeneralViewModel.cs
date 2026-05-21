ae90ef9e7d4f79707ef7d77c35caf63945763058
Astroth <denys.goral@gmail.com>
2026-05-21 21:39:38 +0300

    feat: languages can now be chosen from "Languages" ObservableCollection in GeneralViewModel.cs

--- AstroLauncher/ViewModels/SettingsPageViewModels/GeneralViewModel.cs
+++ AstroLauncher/ViewModels/SettingsPageViewModels/GeneralViewModel.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
@@ -8,9 +9,17 @@ namespace AstroLauncher.ViewModels.SettingsPageViewModels;
 
 public partial class GeneralViewModel : ViewModelBase
 {
+    [ObservableProperty] private string _chosenLanguage;
+    
     public GeneralViewModel()
     {
         DisplayName = "General";
         IconData = (StreamGeometry)Application.Current.FindResource("GeneralIcon");
     }
+    
+    // available language list
+    public ObservableCollection<string> Languages { get; } = new()
+    {
+        "English", "Russian", "Portuguese",
+    };
 }
